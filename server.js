57bebf2d7c1a6863c117b5907fc2a8992547dd04
Arturo Arce <arturo.arce@Arturos-MacBook-Pro.local>
2022-11-07 17:08:37 -0500

    updated express

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express');
 
-
 const app = express();
+const PORT = process.env.PORT || 3001;
 
-app.listen(3001, () => {
-    console.log(`API server now on port 3001!`);
-  });
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json());
+
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
+});
