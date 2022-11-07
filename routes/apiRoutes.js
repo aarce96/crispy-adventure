9817814a91e22ff8387db97d493a70603067d254
Arturo Arce <arturo.arce@Arturos-MacBook-Pro.local>
2022-11-07 18:52:13 -0500

    fix bugs

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -4,15 +4,15 @@ const dataSaved = require("../db/dataSaved");
 router.get("/notes", function (req, res) {
   dataSaved
     .getNotes()
-    .then((notes) => res.json(notes))
-    .catch((err) => res.status(500).json(err));
+    .then(notes => res.json(notes))
+    .catch(err => res.status(500).json(err));
 });
 
 router.post("/notes", function (req, res) {
   dataSaved
     .addNote(req.body)
     .then((note) => res.json(note))
-    .catch((err) => res.status(500).json(err));
+    .catch(err => res.status(500).json(err));
 });
 
 module.exports = router;
