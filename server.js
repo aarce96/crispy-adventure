816729e1dc850454fcfd1b091e99d410c1601f8e
Arturo Arce <arturo.arce@Arturos-MacBook-Pro.local>
2022-11-07 19:06:22 -0500

    push to heroku

--- server.js
+++ server.js
@@ -1,17 +1,38 @@
 const express = require("express");
-const apiRoutes = require("./routes/apiRoutes");
-const htmlRoutes = require("./routes/htmlRoutes");
+const fs = require("fs");
+const path = require("path");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static("public"));
-app.use("./api", apiRoutes);
-app.use("/", htmlRoutes);
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/notes.html"));
+});
+
+app.get("/api/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "/db/db.json"));
+});
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+
+app.post("/api/notes", (req, res) => {
+  let createNote = req.body;
+  let listNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+  let lengthNote = listNotes.length.toString();
+
+  createNote.id = lengthNote;
+  listNotes.push(createNote);
+
+  fs.writeFileSync("./db/db.json", JSON.stringify(listNotes));
+  res.json(listNotes);
+});
 
 app.listen(PORT, () => {
-  console.log(`API server now on port ${PORT}!`);
+  console.log(`API server now on port ${PORT}`);
 });
